319d5ceaecb7ac84f12d752404762c2c62aa5d6c
dhiraj-001 <gogoidhiraj207@gmail.com>
2025-04-11 09:11:16 +0530

    skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -14,6 +14,8 @@ import img10 from "../img/html.png";
 import img11 from "../img/css.png";
 import img12 from "../img/node.png";
 import img13 from "../img/express.png";
+import img14 from "../img/mysql.png";
+import img15 from "../img/mongo.png";
 
 const languages = [
   {
@@ -99,24 +101,71 @@ const frameworks = [
   }
 ];
 
+const techSkills = [
+  {
+    category: "Frontend Development",
+    skills: [
+      { id: 1, name: "HTML", designation: "Advanced", image: img10 },
+      { id: 2, name: "CSS", designation: "Intermediate", image: img11 },
+      { id: 3, name: "JavaScript", designation: "Advanced", image: img9 }
+    ]
+  },
+  {
+    category: "Backend Development",
+    skills: [
+      { id: 4, name: "Node.js", designation: "Backend Development", image: img12 },
+      { id: 5, name: "Express.js", designation: "Backend Development", image: img13 }
+    ]
+  },
+  {
+    category: "Frameworks",
+    skills: [
+      { id: 12, name: "Next.js", designation: "Frontend Framework", image: img3 },
+      { id: 13, name: "React.js", designation: "Frontend Framework", image: img8 },
+      { id: 14, name: "Tailwind CSS", designation: "CSS Framework", image: img6 },
+      { id: 15, name: "Bootstrap", designation: "CSS Framework", image: img7 }
+    ]
+  },
+  {
+    category: "Databases",
+    skills: [
+      { id: 6, name: "MongoDB", designation: "Databases", image: img14 },
+      { id: 7, name: "MySQL", designation: "Databases", image: img15 }
+    ]
+  },
+  {
+    category: "Version Control & Collaboration",
+    skills: [
+      { id: 8, name: "Git", designation: "Version Control", image: img4 },
+      { id: 9, name: "GitHub", designation: "Collaboration", image: img5 }
+    ]
+  },
+  {
+    category: "General Programming Languages",
+    skills: [
+      { id: 10, name: "C", designation: "Advanced", image: img1 },
+      { id: 11, name: "C++", designation: "Intermediate", image: img2 }
+    ]
+  },
+  
+];
+
 export function AnimatedTooltipPreview() {
   return (
     <div className="p-4 flex items-center flex-col w-full mt-5">
       <span className="font-head font-bold text-5xl">My Skills</span>
-      <div className="flex flex-col sml:flex-row gap-8 sml:gap-24 mt-10">
-        <div>
-          <h2 className="font-head text-xl font-bold">Languages</h2>
-          <div className="flex flex-row items-center justify-center mb-10 mt-4 w-full">
-            <AnimatedTooltip items={languages} />
+      <div className="grid mdl:grid-cols-3 xl:grid-flow-col gap-x-[13px] gap-y-0 sml:flex-row gap-5 mdl:gap-8 xl:gap-16 mt-16">
+        {techSkills.map((skill, index) => (
+          <div key={index}>
+            <h2 className="font-head text-xl font-bold sml:h-20 h-4">{skill.category}</h2>
+            <div className="flex flex-row items-center justify-center mb-10 mt-4 w-full">
+              <AnimatedTooltip items={skill.skills} />
+            </div>
           </div>
-        </div>
-        <div>
-          <h2 className="font-head text-xl font-bold">Frameworks</h2>
-          <div className="flex flex-row items-center justify-center mb-10 mt-4 w-full">
-            <AnimatedTooltip items={frameworks} />
-          </div>
-        </div>
+        ))
+        }
+       
       </div>
-    </div>
+    </div >
   );
 }
--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -39,7 +39,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg font-head font-bold md:text-5xl mb-4 text-black dark:text-white max-w-4xl">
+        <h2 className="text-4xl font-head font-bold md:text-5xl mb-4 text-black dark:text-white max-w-4xl">
         Projects & Creations
         </h2>
         <p className="text-neutral-700 dark:text-neutral-300 text-sm font-body md:text-base max-w-sm">
